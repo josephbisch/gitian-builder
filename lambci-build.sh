b898a8703745e01e87bc412ac5b261aa1b4ed8f1
Joseph Bisch <joseph.bisch@gmail.com>
2017-09-04 14:18:52 -0400

    Setup lambci for build

--- lambci-build.sh
+++ lambci-build.sh
@@ -2,4 +2,7 @@
 
 set -e
 
-echo "Build works"
+DEBIAN_FRONTEND=noninteractive apt-get -y update
+DEBIAN_FRONTEND=noninteractive apt-get -y install git ruby openssh-client
+git clone https://github.com/josephbisch/bitcoin.git -b gitian-script-fixes
+bash ./bitcoin/contrib/gitian-build.sh --verify --build --os l --setup --no-commit signer 0.14.2
