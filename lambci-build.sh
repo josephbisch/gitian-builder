cdc719f2a522c9330e5281600a94740053f19e37
Joseph Bisch <joseph.bisch@gmail.com>
2017-09-04 16:23:00 -0400

    Update

--- lambci-build.sh
+++ lambci-build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -e
-
 DEBIAN_FRONTEND=noninteractive apt-get -y update
 DEBIAN_FRONTEND=noninteractive apt-get -y install git ruby openssh-client
 git clone https://github.com/josephbisch/bitcoin.git -b gitian-script-fixes
